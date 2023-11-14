2ead8c9ebe78bbffe60e7b259998a8f3262622bd
KimYongJun <rladydwns129@naver.com>
2023-11-14 16:49:13 +0900

    impl: 지역 상세 화면 이동 구현

--- src/views/TheUserView.vue
+++ src/views/TheUserView.vue
@@ -1,7 +1,10 @@
-<script setup></script>
+<script setup>
+import { RouterView } from "vue-router";
+</script>
 
 <template>
   <div>User 화면입니다.</div>
+  <router-view></router-view>
 </template>
 
 <style scoped></style>
