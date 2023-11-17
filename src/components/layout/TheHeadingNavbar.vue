bd3c26e532618208eb8b84e9302702d2d4c089a2
KimYongJun <rladydwns129@naver.com>
2023-11-17 15:58:34 +0900

    in progress: 지역 상세 화면 제작

--- src/components/layout/TheHeadingNavbar.vue
+++ src/components/layout/TheHeadingNavbar.vue
@@ -1,26 +1,40 @@
 <script setup>
+import { useRouter } from "vue-router";
 import { storeToRefs } from "pinia";
 import { useMenuStore } from "../../stores/menu";
 import { useAuthStore } from "../../stores/auth";
 
+const router = useRouter();
 const menuStore = useMenuStore();
 const authStore = useAuthStore();
 const { menuList } = storeToRefs(menuStore);
 const { changeMenuState } = menuStore;
+
 const logout = () => {
   console.log("logout!!!!!");
   authStore.logout();
   changeMenuState();
 };
+
+const moveList = () => {
+  router.push({ name: "whole-article-list" });
+};
+
+const moveMain = () => {
+  router.push({ name: "main" });
+};
 </script>
 
 <template>
   <v-app-bar :elevation="1">
-    <v-app-bar-title style="cursor: pointer"> Artventure </v-app-bar-title>
+    <v-app-bar-title style="cursor: pointer" @click="moveMain">
+      Artventure
+    </v-app-bar-title>
+    <v-btn @click="moveList">게시판</v-btn>
     <template v-for="menu in menuList" :key="menu.routeName">
       <template v-if="menu.show">
         <template v-if="menu.routeName === 'user-logout'">
-          <v-avatar color="info"> <span class="text-h5">pic</span></v-avatar>
+          <v-avatar color="info"><span class="text-h5">KYJ</span></v-avatar>
           <v-btn
             ><router-link to="/" @click.prevent="logout">{{
               menu.name
